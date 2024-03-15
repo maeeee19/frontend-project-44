236fcfffaf143b18cf640809d7d5e07a5be4d0d7
root <root@DESKTOP-4K3CG6Q>
2024-03-15 23:41:13 +0300

    bin

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export function greetings () {
+function greetings () {
     console.log('brain-games');
     console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have your name?: ');
     console.log(`Hello, ${name}!`);
 };
+
+export default greetings;

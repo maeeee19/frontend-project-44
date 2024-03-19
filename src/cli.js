1a291c4f1d3091e215fc0bb54d00bc73f02f0f36
root <root@DESKTOP-4K3CG6Q>
2024-03-20 00:55:38 +0300

    tab

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 function greetings() {
-    console.log('brain-games');
+	console.log('brain-games');
     console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have your name?: ');
     console.log(`Hello, ${name}!`);
